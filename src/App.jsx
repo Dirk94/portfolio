003ad6596892b2f52856e66e69c10d3189a53849
Dirk Hoekstra <dirkhoekstra@gmail.com>
2021-01-05 17:43:56 +0100

    setup the welcome page

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,25 @@ import {
 } from 'react-router-dom'
 import HomePage from './home/HomePage'
 import PortfolioPage from './portfolio/PortfolioPage'
+import './App.scss'
 
 function App () {
   return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route path={'/portfolio'}>
-            <PortfolioPage />
-          </Route>
-          <Route path={'/'}>
-            <HomePage />
-          </Route>
-        </Switch>
-      </Router>
+      <div className="h-100vh d-flex align-items-center">
+    <div className="container">
+      <div className="row">
+        <Router>
+          <Switch>
+            <Route path={'/portfolio'}>
+              <PortfolioPage />
+            </Route>
+            <Route path={'/'}>
+              <HomePage />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </div>
     </div>
   )
 }
--- src/home/HomePage.jsx
+++ src/home/HomePage.jsx
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './HomePage.scss'
+import Me from '../assets/me.jpg'
+import useInterval from '../utils/interval'
 
-function HomePage () {
+const finalTitle = 'I\'m a Full-Stack Developer'
+
+const HomePage = (props) => {
+  const [title, setTitle] = useState('')
+
+  useInterval(() => {
+    if (title.length === finalTitle.length) {
+      return
+    }
+    setTitle(title + finalTitle.charAt(title.length))
+  }, 40)
+
   return (
-        <h1>Home</h1>
+        <>
+          <div className={'col-md-4'}>
+            <div className={'main-profile-image'}>
+                <img src={Me} alt="Picture of me" />
+            </div>
+          </div>
+          <div className={'col-md-8 col-lg-6 align-self-center main-text'}>
+              <span className="greeting">Hi 👋</span>
+              <h1 className={'name'}><span className={'accent'}>Dirk Hoekstra</span></h1>
+              <h2 className="header-title">
+                  { title }
+                  {title.length !== finalTitle.length && '|'}
+              </h2>
+
+              <p className={'short-info'}>
+                  5+ years of work experience. My favorite stack is Vue with Laravel,
+                  but I&apos;m happy to use other technologies.
+                  Founder of 3 tech companies of which 1 was acquired in 2020.
+              </p>
+
+              <button className={'btn btn-primary btn-portfolio'} href={'#'}>
+                  <i className="fas fa-briefcase"></i>
+                  Portfolio
+              </button>
+          </div>
+        </>
   )
 }
 
